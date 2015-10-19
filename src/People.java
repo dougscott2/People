a3091068a37d761fbb4b5c916fb54a8806569eb5
DrScott <dougscott2@gmail.com>
2015-10-19 13:46:23 -0400

    build1.0

--- src/People.java
+++ src/People.java
@@ -1,8 +1,77 @@
+import com.sun.xml.internal.bind.v2.runtime.RuntimeUtil;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 /**
  * Created by DrScott on 10/19/15.
  */
 public class People {
     public static void main(String[] args) {
+        HashMap<String, ArrayList<Person>> map = new HashMap();
+        String fileContent = readFile("people.csv");
+        String[] lines = fileContent.split("\n"); //
+        String names;
 
+
+        for (String line : lines) {
+
+            String[] columns = line.split(",");
+            String id = columns[0];
+            String firstName = columns[1];
+            String lastName = columns[2];
+            String email = columns[3];
+            String country = columns[4];
+            String ipAddress = columns[5];
+
+            Person p = new Person(firstName, lastName, country); //new country object is created with the abbreviation and name
+            String name = String.valueOf(lastName);
+            ArrayList<Person> list = map.get(country);
+
+                 if (list == null) {
+                     list = new ArrayList();
+                     list.add(p);
+                     map.put(country, list);
+                 } else {
+                     list.add(p);
+                 }
+
+
+            Collections.sort(list);
+
+        }//for
+
+        System.out.println(map.toString());
+    }//end main
+
+
+
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        } catch (Exception e) {
+            return null;
+        }
     }
-}
+
+    static void writeFile(String fileName, String fileContent){
+        File f = new File(fileName);
+        try{
+            FileWriter fw = new FileWriter(f);
+            fw.write(fileContent);
+            fw.close();
+        } catch (Exception e){
+
+        }
+    }
+}//end class
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,50 @@
 /**
  * Created by DrScott on 10/19/15.
  */
-public class Person {
+public class Person implements Comparable{
+    String id;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ipAddress;
+
+    public Person(String firstName, String lastName, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        //this.email = address;
+        this.country = country;
+        //this.ipAddress = ipAddress;
+    }
+    public String toString(){
+        return String.format("%s %s from %s \n", firstName, lastName, country);
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Person p = (Person) o;
+        int result = lastName.compareTo(p.lastName);
+        return result;
+    }
 }
+
+
