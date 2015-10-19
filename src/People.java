ebab46f23e9c43e10a61a6179c1dd5e078644109
DrScott <dougscott2@gmail.com>
2015-10-19 12:12:46 -0400

    initial import

---
+++ src/People.java
@@ -0,0 +1,8 @@
+/**
+ * Created by DrScott on 10/19/15.
+ */
+public class People {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,5 @@
+/**
+ * Created by DrScott on 10/19/15.
+ */
+public class Person {
+}
